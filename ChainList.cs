c166827e3672e2a07966503ead302dc1dc25ae09
MilenaSkrt <135116799+MilenaSkrt@users.noreply.github.com>
2024-03-12 15:01:09 +0500

    Update ChainList.cs

--- ChainList.cs
+++ ChainList.cs
@@ -1,167 +1,118 @@
 class ChainList
 {
-    public class Node
-    {
-        public int Data { get; set; }
+    public class Node // Внутренний класс для представления узла списка
+	{
+        public int Data { get; set; } // Данные в узле
 
-        public Node Next { get; set; }
+		public Node Next { get; set; } // Ссылка на следующий узел
 
-        public Node(int data, Node next)
-        {
+		public Node(int data, Node next) // Конструктор узла с заданными данными и ссылкой на следующий узел
+		{
             Data = data;
             Next = next;
 
         }
     }
+	// Ссылка на начало списка
+	Node head = null;
+    int count = 0; // Количество элементов в списке
 
-    Node head = null;
-    int count = 0;
 
-    public Node Find(int position)
-    {
-        if (position >= count || position < 0) { return null; }
-        int i = 0;
+	public Node Find(int position) // Метод для поиска узла по заданной позиции
+	{
+        if (position >= count || position < 0) { return null; } // Если позиция некорректна, вернуть null
+		int i = 0;
         Node p = head;
-        while (p != null && i < position)
-        {
+        while (p != null && i < position) // Перебор узлов до заданной позиции
+		{
             p = p.Next;
             i++;
         }
 
-        if (i == position) { return p; }
-        else { return null; }
-        
-    }
+        if (i == position) { return p; } // Вернуть найденный узел
+		else { return null; } // Вернуть null, если узел не найден
 
-    public void Add(int value)
-    {
-        if (head == null || count == 0) { head = new Node(value, null); }
-        else
+	}
+
+    public void Add(int value) // Метод для добавления нового элемента в конец списка
+	{
+        if (head == null || count == 0) { head = new Node(value, null); } // Если список пуст, создать новый узел в начале
+		else
         {
 
-            Find(count - 1).Next = new Node(value, null);
+            Find(count - 1).Next = new Node(value, null); // Добавить новый узел в конец списка
 
-        }
-        count++;
-    }
+		}
+        count++; // Увеличить количество элементов в списке
+	}
 
-    public void Insert(int value, int position)
-    {
-        if (position < count && position > 0)
-        {
+    public void Insert(int value, int position) // Метод для вставки нового элемента на заданную позицию
+	{
+        if (position < count && position > 0) // Если позиция валидна
+		{
 
             Node OldNode = Find(position - 1);
             Node NewNode = new Node(value, OldNode.Next);
-            OldNode.Next = NewNode;
+            OldNode.Next = NewNode; // Вставить новый узел на указанную позицию
 
-        }
-        else 
+		}
+		else // Если позиция некорректна
+		{
+
+            if (position == 0) { head = new Node(value, head); } // Вставить новый узел в начало списка
+			else { Add(value); count--; } // Добавить новый узел в конец списка
+
+		}
+        count++;// Увеличить количество элементов в списке
+	}
+
+    public void Clean() // Метод для очистки списка
+	{
+
+        head = null;// Удалить все узлы
+		count = 0;// Обнулить количество элементов
+
+	}
+
+    public void Delete(int position) // Метод для удаления элемента на заданной позиции
+	{
+
+        if (count == 1 && position == 0) { Clean(); } // Если в списке только один элемент
+		else
         {
 
-            if (position == 0) { head = new Node(value, head); }
-            else { Add(value); count--; }
+            if (position < count && position >= 0) // Если позиция валидна
+			{
+                if (position == 0) { head = head.Next; } // Удалить первый узел
+
+				else { Find(position - 1).Next = Find(position + 1); } // Удалить узел на заданной позиции
+
+				count--; // Уменьшить количество элементов в списке
+			}
 
         }
-        count++;
+    }
+        
+    public int Count  // Свойство для получения количества элементов в списке
+	{
+        get { return count; } 
     }
 
-    public void Clean() 
-    {
 
-        head = null;
-        count = 0;
-
-    }
-
-    public void Delete(int position)
-    {
-
-        if (count == 1 && position == 0) { Clean(); }
-        else
-        {
-
-            if (position < count && position >= 0)
-            {
-                if (position == 0) { head = head.Next; }
-
-                else { Find(position - 1).Next = Find(position + 1); }
-
-                count--;
-            }
-            
-        }
-        // if (position >= 0 && position < count - 1) 
-        // { 
-
-        //    if (position == 0) { head = head.Next; }
-        //    else
-        //    {
-
-        //        Find(position - 1).Next = Find(position + 1); 
-
-        //   }
-        //   count--;
-
-        //}
-    }
-    public int Count
-    {
-        get { return count; }
-    }
-
-    public int this[int position]
-    {
-        get
-        {
-            if (position >= count || position < 0) { return 0; }
-            else { return Find(position).Data; }
-        }
-
-        set
-        {
-            if (position < count && position >= 0) { Find(position).Data = value; }
-        }
-    }
-
-    public void Print()
-    {
+    public void Print() // Метод для вывода элементов списка
+	{
         Console.Write("[");
-        Node p = head;
-        for (int i = 0; i < count; i++)
-        {
+        Node p = head; // Устанавливаем указатель p на головной узел
+		for (int i = 0; i < count; i++)  // Проходим по всем элементам списка
+		{
 
-            if (i == count - 1) { Console.Write(p.Data); }
-            else { Console.Write($"{p.Data}, "); }
-            p = p.Next;
+            if (i == count - 1) { Console.Write(p.Data); } // Если текущий элемент - последний, выводим его значение без запятой
+			else { Console.Write($"{p.Data}, "); } // Иначе выводим значение элемента с запятой и пробелом
+			p = p.Next; // Переходим к следующему элементу
 
-        }
+		}
         Console.WriteLine("]");
     }
 
-    public int Moda
-    {
-        get
-        {
-            if (count == 0) return -1;
-            int maxCount = 0;
-            int moda = 0;
-            for (int i = 0; i < this.count; i++)
-            {
-                int count = 0;
-                int item = Find(i).Data;
-
-                for (int j = 0; j < this.count; j++)
-                    if (item == Find(j).Data) count++;
-
-
-                if (count > maxCount)
-                {
-                    maxCount = count;
-                    moda = item;
-                }
-            }
-            return moda;
         }
-    }
-}
-    
+  
